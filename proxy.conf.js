3d974bb54efd37e9407059e75e07980ce9f5f207
Robin Coma Delperier <robin@placeme.io>
2021-09-08 09:28:05 +0200

    feat(ADF): trial url

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,7 +2,7 @@
 module.exports = {
   "/alfresco": {
     // "target": "http://localhost:8080",
-    "target": "https://zdoqv786.trials.alfresco.com",
+    "target": "https://sjxlxful.trials.alfresco.com",
     "secure": false,
     "changeOrigin": true
   }
